9362c280eaf50ce9bac036449e515a935ec8b7ab
luis-blc <luisfaria@bluelightcard.co.uk>
2024-05-27 16:02:27 +0100

    added company ranking

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as RequestRideForm } from './requestRideForm';
 export { default as IphoneFrame } from './iphoneFrame';
 export { default as FakeAppIcon } from './fakeAppIcon';
-export { default as rideModal } from './/rideModal';
+export { default as RideModal } from './rideModal';
+export { default as CompanyRankingModal } from './companyRankingModal';
