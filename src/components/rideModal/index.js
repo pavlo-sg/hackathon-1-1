806360e9f4e9989c85aecc876ac1092459279827
Pavlo Omelianchuk <pavlo.omelianchuk@gmail.com>
2024-05-27 17:03:30 +0100

    feat: accept user input

--- src/components/rideModal/index.js
+++ src/components/rideModal/index.js
@@ -1,33 +1,34 @@
 // src/components/RideModal.js
-import React from 'react';
-import Modal from 'react-modal';
-import { useState } from 'react';
-import CompanyRankingModal from '../companyRankingModal'
-
+import React from "react";
+import Modal from "react-modal";
+import { useState } from "react";
+import CompanyRankingModal from "../companyRankingModal";
+import useDistanceMatrix from "../../hooks/useDistanceMatrix";
 const modalStyles = {
   overlay: {
-    backgroundColor: 'rgba(0, 0, 0, 0.5)'
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
   },
   content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    borderRadius: '20px',
-    border: 'none',
-    padding: '20px',
-    width: '80%', // Adjust the width as needed
-    height: '80%', // Adjust the height as needed
-    maxWidth: '300px', // Maximum width
-    maxHeight: '300px', // Maximum height
-    overflow: 'auto'
-  }
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    borderRadius: "20px",
+    border: "none",
+    padding: "20px",
+    width: "80%", // Adjust the width as needed
+    height: "80%", // Adjust the height as needed
+    maxWidth: "300px", // Maximum width
+    maxHeight: "300px", // Maximum height
+    overflow: "auto",
+  },
 };
-const RideModal = ({ isOpen, onRequestClose }) => {
-      const [companyRankingModalIsOpen, setCompanyRankingModalIsOpen] = useState(false);
-
+const RideModal = ({ isOpen, onRequestClose, pickup, dropoff }) => {
+  const { distance, duration } = useDistanceMatrix(pickup, dropoff);
+  const [companyRankingModalIsOpen, setCompanyRankingModalIsOpen] = useState(false);
+  console.log(distance);
   const openCompanyRankingModal = () => {
     setCompanyRankingModalIsOpen(true);
   };
@@ -36,17 +37,23 @@ const RideModal = ({ isOpen, onRequestClose }) => {
     setCompanyRankingModalIsOpen(false);
   };
   return (
-    <Modal
-      isOpen={isOpen}
-      onRequestClose={onRequestClose}
-      contentLabel="Request Ride Modal"
-      style={modalStyles}
-    >
+    <Modal isOpen={isOpen} onRequestClose={onRequestClose} contentLabel="Request Ride Modal" style={modalStyles}>
       <h2>Request Ride</h2>
+      <div>
+        <h1>Distance Matrix</h1>
+        {distance && duration ? (
+          <div>
+            <p>Distance: {distance}</p>
+            <p>Duration: {duration}</p>
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
       <p>Save up to xx co2 carbon footprint by using different transportation...</p>
       <button onClick={onRequestClose}>Close Modal</button>
       <button onClick={openCompanyRankingModal}>Company Ranking</button>
-      <CompanyRankingModal isOpen={companyRankingModalIsOpen} onRequestClose={closeCompanyRankingModal} />  
+      <CompanyRankingModal isOpen={companyRankingModalIsOpen} onRequestClose={closeCompanyRankingModal} />
     </Modal>
   );
 };
